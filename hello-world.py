f05c8f74ba6da9f9f53683ff2f66d08353c0c713
an0wen <artyomaguichine@gmail.com>
2024-01-04 04:50:19 -0800

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+# This function tells the computer to print Hello World!
+def PrintHelloWorld():
+    print("Hello World!")
+
+# This defines ou main() function for our program
+def main():
+    PrintHelloWorld()
+
+# When we run the program, this exectues first
+if __name__=="__main__":
+    main()
